bbae0c15e7134aca2d3977fb8442a39d853b7b91
dinhxuan <dinhxuandx123456@gmail.com>
2025-05-26 15:42:44 +0700

    update

---
+++ Lab02/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from cipher.caesar import CaesarCipher
+
+app = Flask(__name__)
+
+# Router for home page
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+# Router for Caesar cipher page
+@app.route("/caesar")
+def caesar():
+    return render_template('caesar.html')
+
+# Router to handle encryption POST request
+@app.route("/encrypt", methods=['POST'])
+def caesar_encrypt():
+    text = request.form['inputPlainText']
+    key = int(request.form['inputkeyPlain'])
+    caesar = CaesarCipher()
+    encrypted_text = caesar.encrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+# Router to handle decryption POST request
+@app.route("/decrypt", methods=['POST'])
+def caesar_decrypt():
+    text = request.form['inputCipherText']
+    key = int(request.form['inputKeyCipher'])
+    caesar = CaesarCipher()
+    decrypted_text = caesar.decrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+# Main function to run app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, debug=True)
